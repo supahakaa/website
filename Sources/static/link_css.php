93500de934643a149e4078fdc43eebc0fd6975e5
Sawe <sawealexander@gmail.com>
2022-07-29 21:26:44 +0300

    initial

---
+++ Sources/static/link_css.php
@@ -0,0 +1,28 @@
+<link rel="icon" 
+      type="image/png" 
+      href="<?php echo $base_url ;?>Sources/static/img/minArchvilla.png">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="<?php echo $base_url ;?>Sources/static/css/bootstrap.min.css">
+<!-- Material Design Bootstrap -->
+<link rel="stylesheet" href="<?php echo $base_url ;?>Sources/static/css/mdb.min.css">
+<!-- Custom styles -->
+<style>
+    * {
+ font-size: 100%;
+ font-family: poppins;
+}
+.bold_font{
+    font-size: 100%;
+ font-family: poppins_bold;
+}
+    @font-face {
+  font-family: 'poppins';
+  src: url('<?php echo $base_url ;?>Sources/static/font/poppins/Poppins-ExtraLight.ttf')  format('truetype'); /* Legacy iOS */
+}
+    @font-face {
+  font-family: 'poppins_bold';
+  src: url('<?php echo $base_url ;?>Sources/static/font/poppins/Poppins-SemiBold.ttf')  format('truetype'); /* Legacy iOS */
+}
+</style>
---
+++ Sources/static/link_js.php
@@ -0,0 +1,9 @@
+  <!-- SCRIPTS -->
+  <!-- JQuery -->
+  <script src="<?php echo $base_url ;?>Sources/static/js/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="<?php echo $base_url ;?>Sources/static/js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="<?php echo $base_url ;?>Sources/static/js/bootstrap.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="<?php echo $base_url ;?>Sources/static/js/mdb.min.js"></script>
